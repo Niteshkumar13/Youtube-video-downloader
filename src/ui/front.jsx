ebc0f17adcaee37313c825641b6fc33a10fe41e6
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2023-10-04 08:32:26 +0530

    Update front.jsx

--- src/ui/front.jsx
+++ src/ui/front.jsx
@@ -32,7 +32,7 @@ if(event.target.value === null){
    const options = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': process.env.API_URL,
+      'X-RapidAPI-Key': os.envoirement("API_URL"),
         'X-RapidAPI-Host': 'ytstream-download-youtube-videos.p.rapidapi.com'
     }
 };
