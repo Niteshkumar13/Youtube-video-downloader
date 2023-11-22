9340dfa866d7f511d0f7a36d717209efc6491829
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2023-11-22 22:56:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Front from './ui/front';
 import avtar from './Avatar-Profile-PNG-Photos.png';
-import { inject } from '@vercel/analytics';
+import { Analytics } from '@vercel/analytics/react';
 function App() {
   return (
     <>
     
 <inject/>
     <div className='body'>
- 
+  <Analytics />
     <Front/>
     <div className='bottom-nav'><h2 style={{marginRight:"20px"}}>Made by-</h2>
     <img className='bottom-image' alt="avtar" src={avtar}/>
